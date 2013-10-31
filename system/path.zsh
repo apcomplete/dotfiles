8e91c2c66667bf6f4fd446f6955c2e48fc668a81
Alex Padgett <padgett.alex@gmail.com>
2013-10-31 10:36:25 -0400

    Clean up path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,11 @@
-export PATH="vendor/ruby/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+BIN_PATH=./vendor/ruby/bin:./.bin:./bin
+
+RUBY_PATH=vendor/ruby/bin:$HOME/.rbenv/shims:$HOME/.rbenv/bin
+
+LOCAL_PATH=/usr/local/bin:/usr/local/sbin
+
+ZSH_PATH=$ZSH/bin
+
+export PATH=$BIN_PATH:$RUBY_PATH:$LOCAL_PATH:$ZSH_PATH:$PATH
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
