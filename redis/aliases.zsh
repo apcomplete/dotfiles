bdb646733e162bbbb06a59010799f991b4c1a980
Alex Padgett <padgett.alex@gmail.com>
2013-11-11 10:21:35 -0500

    Add redis config

---
+++ redis/aliases.zsh
@@ -0,0 +1,2 @@
+alias redis-up='redis-server ~/.dotfiles/redis/redis.conf'
+alias redis-down='cat /usr/local/var/run/redis.pid | xargs kill -15'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,3 @@
 alias reload!='. ~/.zshrc'
 alias rspec='nocorrect rspec'
 alias cap='nocorrect cap'
-alias redis-up='redis-server /usr/local/etc/redis.conf'
