aea036109f8d2d1cfcc906adffe0b4aedb1baf7f
Alex Padgett <padgett.alex@gmail.com>
2022-05-11 21:08:28 -0400

    Add local bin to path

--- system/path.zsh
+++ system/path.zsh
@@ -8,6 +8,6 @@ LOCAL_PATH=/usr/local/bin:/usr/local/sbin
 
 ZSH_PATH=$ZSH/bin
 
-export PATH=$BIN_PATH:$RUBY_PATH:$NPM_PATH:$LOCAL_PATH:$ZSH_PATH:$PATH
+export PATH=$BIN_PATH:$RUBY_PATH:$NPM_PATH:$LOCAL_PATH:$ZSH_PATH:$HOME/.local/bin:$PATH
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
