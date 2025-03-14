e907e6499a4472e021503c0f6b2f5359828456a2
apcomplete <padgett.alex@gmail.com>
2025-03-14 10:37:04 -0400

    Update

--- exports.zsh
+++ exports.zsh
@@ -8,7 +8,7 @@ LOCAL_PATH=/usr/local/bin:/usr/local/sbin
 
 NVIM_PATH="$PATH:/opt/nvim-linux64/bin"
 
-export PATH=$BIN_PATH:$NVIM_PATH:$RUBY_PATH:$NPM_PATH:$LOCAL_PATH:$HOME/.local/bin:$PATH
+export PATH=$RUBY_PATH:$NVIM_PATH:$NPM_PATH:$BIN_PATH:$LOCAL_PATH:$HOME/.local/bin:$PATH
 
 export PROJECTS=~/Projects
 
@@ -20,5 +20,5 @@ export CLICOLOR=true
 export EDITOR='nvim -N'
 
 # KERL config for setting up erlang
-export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --enable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit"
+export KERL_CONFIGURE_OPTIONS="--disable-debug --disable-silent-rules --without-javac --enable-shared-zlib --enable-dynamic-ssl-lib --enable-hipe --disable-sctp --enable-smp-support --enable-threads --enable-kernel-poll --enable-wx --enable-darwin-64bit"
 
