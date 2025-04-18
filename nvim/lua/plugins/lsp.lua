f4731729ff9cae7117fea7a4431c756cdd286780
apcomplete <padgett.alex@gmail.com>
2025-04-18 15:08:42 +0000

    Update ElixirLS and use leader instead of space

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,14 +17,15 @@ return {
         nextls = { enable = false },
         elixirls = {
           enable = true,
+          tag = "v0.27.2",
           settings = elixirls.settings {
             dialyzerEnabled = false,
             enableTestLenses = false,
           },
           on_attach = function(client, bufnr)
-            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<LEADER>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<LEADER>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("v", "<LEADER>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
           end,
         },
         projectionist = {
