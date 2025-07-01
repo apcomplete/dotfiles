f74b57b67295cfceeb96b1bf97b2ec39611e553f
apcomplete <padgett.alex@gmail.com>
2025-07-01 17:56:04 +0000

    Fix pipe manipulation functions

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,31 +1,19 @@
-local M = {}
-
 local get_cursor_position = function()
-  local rowcol = vim.api.nvim_win_get_cursor(0)
-  local row = rowcol[1] - 1
-  local col = rowcol[2]
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
 
-  return row, col
+  -- vim.api.nvim_win_get_cursor() returns a (1,0)-indexed, buffer-relative cursor position for a given window
+  -- i.e. rows are 1-based, cols are 0-based
+  -- while the LSP expects a 0-based cursor position
+  return row - 1, col
 end
 
-local manipulate_pipes = function(direction, client)
-  local row, col = get_cursor_position()
-
-  client.request_sync("workspace/executeCommand", {
-    command = "manipulatePipes:serverid",
-    arguments = { direction, "file://" .. vim.api.nvim_buf_get_name(0), row, col },
-  }, nil, 0)
-end
-
-function M.from_pipe(client)
+local manipulate_pipes = function(direction)
   return function()
-    manipulate_pipes("fromPipe", client)
-  end
-end
-
-function M.to_pipe(client)
-  return function()
-    manipulate_pipes("toPipe", client)
+    local row, col = get_cursor_position()
+    vim.lsp.buf.execute_command({
+      command = "manipulatePipes:serverid",
+      arguments = { direction, "file://" .. vim.api.nvim_buf_get_name(0), row, col },
+    })
   end
 end
 
@@ -86,24 +74,17 @@ return {
       })
 
       vim.lsp.enable('elixirls')
-      vim.lsp.enable('nextls')
+      -- vim.lsp.enable('nextls')
       vim.lsp.enable('lua_ls')
 
       -- Use internal formatting for bindings like gq. null-ls or neovim messes this up somehow
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspAttach", { clear = true }),
         callback = function(ev, bufopts)
-          local bufnr = vim.api.nvim_get_current_buf()
-          local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
-          vim.api.nvim_buf_create_user_command(bufnr, "ElixirFromPipe", M.from_pipe(client), {})
-          vim.api.nvim_buf_create_user_command(bufnr, "ElixirToPipe", M.to_pipe(client), {})
-
           vim.bo[ev.buf].formatexpr = nil
           vim.lsp.completion.enable(true, ev.data.client_id, ev.buf)
-          vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("n", "<leader>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("v", "<leader>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("n", "<leader>fp", manipulate_pipes("fromPipe"), { buffer = true, noremap = true })
+          vim.keymap.set("n", "<leader>tp", manipulate_pipes("toPipe"), { buffer = true, noremap = true })
           vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
           vim.keymap.set("n", "<C-]>", vim.lsp.buf.definition, bufopts)
